0b151defeeb365e97b5928b915d6807f908a98f2
mrtenkorang <josephoforitenkorang390@gmail.com>
2025-03-03 16:05:15 +0000

    m

--- lib/view/splash/splash.dart
+++ lib/view/splash/splash.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'package:greenfarmer/utils/colors.dart';
+import 'package:greenfarmer/view/widgets/app_button.dart';
+import 'package:greenfarmer/view/widgets/app_text.dart';
+import 'package:get/get.dart';
+import 'package:greenfarmer/view/auth/auth_wrapper.dart';
 
 class Splash extends StatefulWidget {
   const Splash({super.key});
@@ -12,7 +16,49 @@ class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Container(
+        height: double.maxFinite,
+        width: double.maxFinite,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/img/splash_image.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            SizedBox(height: 100),
+            Column(
+              children: [
+                AppText(
+                  text: "Green Farmer",
+                  color: AppColors.whiteColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                ),
+                AppText(
+                  text: "We farm, you buy",
+                  color: AppColors.whiteColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ],
+            ),
+            Spacer(),
+            AppButton(
+              buttonWidth: 300,
+              buttonTextColor: AppColors.whiteColor,
+              onPressed: () {
+                Get.to(() => Login());
+              },
+              buttonText: "Get Started",
+              buttonTextFontWeight: FontWeight.bold,
+              backgroundColor: AppColors.lightWhiteColor.withAlpha(350),
+            ),
+            SizedBox(height: 100),
+          ],
+        ),
+      ),
     );
   }
 }
