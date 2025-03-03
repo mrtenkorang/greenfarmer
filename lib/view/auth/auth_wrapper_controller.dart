8d9247c090aad0465351d6384b129e0d163a4120
mrtenkorang <josephoforitenkorang390@gmail.com>
2025-03-03 16:04:30 +0000

    m

---
+++ lib/view/auth/auth_wrapper_controller.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AuthLoginSignupController extends GetxController {
+  var activeTabIndex = 0.obs;
+  TabController? tabController;
+}
