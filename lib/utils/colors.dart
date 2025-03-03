a5364107daae1a4ff58b6caeaf0e1c7f69d4e6be
mrtenkorang <josephoforitenkorang390@gmail.com>
2025-03-03 17:38:12 +0000

    m

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -3,4 +3,8 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const whiteColor = Color(0xFFffffff);
   static const lightWhiteColor = Color(0xFFD9D9D9);
+
+  static const kPrimaryColor = Color(0xFF018241);
+  static const kSecondaryColor = Color(0xFF5E5E5E);
+  static const kTextColor = Color(0xFF5E5E5E);
 }
