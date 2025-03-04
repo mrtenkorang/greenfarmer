cc064a7259b18861a939133771da6fa2c71abc3e
mrtenkorang <josephoforitenkorang390@gmail.com>
2025-03-04 09:23:07 +0000

    m

--- lib/view/auth/auth_wrapper.dart
+++ lib/view/auth/auth_wrapper.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:greenfarmer/utils/colors.dart';
 import 'package:greenfarmer/view/auth/login/login.dart';
+import 'package:greenfarmer/view/auth/signup/signup.dart';
+import 'package:greenfarmer/view/util/images.dart';
 
 import '../widgets/app_text.dart';
 import 'auth_wrapper_controller.dart';
@@ -22,66 +24,64 @@ class AuthWrapper extends StatelessWidget {
               width: double.maxFinite,
               decoration: BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage("assets/img/auth_image.jpg"),
+                  image: AssetImage(AppImages.authImage),
                   fit: BoxFit.cover,
                 ),
               ),
             ),
 
-            Container(
-              child: Obx(
-                () => TabBar(
-                  onTap: (index) {
-                    authController.activeTabIndex.value = index;
-                  },
-                  //labelColor: AppColors,
-                  unselectedLabelColor: Colors.black87,
-                  indicatorSize: TabBarIndicatorSize.label,
-                  indicator: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        width: 2.0,
-                        //color: AppColors.kSecondaryColor,
+            Obx(
+              () => TabBar(
+                onTap: (index) {
+                  authController.activeTabIndex.value = index;
+                },
+                //labelColor: AppColors,
+                unselectedLabelColor: Colors.black87,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicator: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      width: 2.0,
+                      //color: AppColors.kSecondaryColor,
+                    ),
+                  ),
+                ),
+                controller: authController.tabController,
+                tabs: [
+                  Tab(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: AppText(
+                        text: 'Login',
+                        color: Colors.black,
+                        fontWeight:
+                            authController.activeTabIndex.value == 0
+                                ? FontWeight.bold
+                                : FontWeight.w400,
                       ),
                     ),
                   ),
-                  controller: authController.tabController,
-                  tabs: [
-                    Tab(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                        child: AppText(
-                          text: 'Login',
-                          color: Colors.black,
-                          fontWeight:
-                              authController.activeTabIndex.value == 0
-                                  ? FontWeight.bold
-                                  : FontWeight.w400,
-                        ),
+                  Tab(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: AppText(
+                        text: 'Signup',
+                        color: Colors.black,
+                        fontWeight:
+                            authController.activeTabIndex.value == 1
+                                ? FontWeight.bold
+                                : FontWeight.w400,
                       ),
                     ),
-                    Tab(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                        child: AppText(
-                          text: 'Signup',
-                          color: Colors.black,
-                          fontWeight:
-                              authController.activeTabIndex.value == 1
-                                  ? FontWeight.bold
-                                  : FontWeight.w400,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
 
             Expanded(
               child: TabBarView(
                 controller: authController.tabController,
-                children: [Login(), Container()],
+                children: [Login(), Signup()],
               ),
             ),
           ],
