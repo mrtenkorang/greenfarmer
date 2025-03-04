2a82f114487c5cf2b24aa693c0e71c0b4ae49be3
mrtenkorang <josephoforitenkorang390@gmail.com>
2025-03-04 09:31:25 +0000

    m

---
+++ lib/view/auth/signup/signup.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:greenfarmer/view/auth/login/login_controller.dart';
+import 'package:greenfarmer/view/util/images.dart';
+import 'package:greenfarmer/view/widgets/app_button.dart';
+import 'package:greenfarmer/view/widgets/app_text.dart';
+import 'package:greenfarmer/view/widgets/app_text_field.dart';
+import 'package:get/get.dart';
+import 'package:greenfarmer/utils/colors.dart';
+import 'package:greenfarmer/view/widgets/third_party_auth_container.dart';
+
+class Signup extends StatelessWidget {
+  const Signup({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final loginController = Get.put(LoginController());
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0, top: 10.0, right: 10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AppText(text: 'Signup to continue'),
+          SizedBox(height: 20),
+          AppTextField(hintText: "Full name"),
+          SizedBox(height: 20),
+          AppTextField(hintText: "Username"),
+          SizedBox(height: 20),
+          AppTextField(
+            suffix: IconButton(
+              onPressed: () {
+                loginController.togglePasswordVisibility();
+              },
+              icon:
+              loginController.hidePassword.value
+                  ? Icon(Icons.visibility)
+                  : Icon(Icons.visibility_off),
+            ),
+            obscureText: loginController.hidePassword.value,
+            hintText: "Password",
+          ),
+
+          SizedBox(height: 30),
+          AppButton(
+            backgroundColor: AppColors.kPrimaryColor,
+            onPressed: () {},
+            buttonText: "Signup",
+            buttonTextFontWeight: FontWeight.bold,
+            buttonTextColor: AppColors.whiteColor,
+            buttonWidth: double.maxFinite,
+          ),
+          SizedBox(height: 10,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(),
+              // Row(
+              //   children: [
+              //     GetBuilder(
+              //       init: loginController,
+              //       builder: (_) {
+              //         return GestureDetector(
+              //           onTap: () {
+              //             loginController.toggleRememberMe(
+              //                 !loginController.rememberMe.value);
+              //           },
+              //           child: Checkbox(
+              //             overlayColor: WidgetStateProperty.all(
+              //               loginController.rememberMe.value
+              //                   ? context.theme.colorScheme.primary
+              //                   : Colors.transparent,
+              //             ),
+              //             value: loginController.rememberMe.value,
+              //             onChanged: (value) {
+              //               loginController.toggleRememberMe(value!);
+              //             },
+              //           ),
+              //         );
+              //       },
+              //     ),
+              //     AppText(
+              //       text: "Remember me",
+              //       color: AppColors.kTextColor,
+              //     ),
+              //   ],
+              // ),
+              AppText(
+                text: "Forgot Password?",
+                color: AppColors.kPrimaryColor,
+              ),
+
+
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(child: Divider(height: 5,)),
+              SizedBox(width: 10,),
+              AppText(text: "GF", fontWeight: FontWeight.bold,),
+              SizedBox(width: 10,),
+              Expanded(child: Divider(height: 5,)),
+            ],
+          ),
+          SizedBox(height: 20,),
+          ThirdPartyAuthContainer(children: [
+            Row(
+
+              children: [
+                Image.asset(AppImages.googleLogo, height: 20,),
+                SizedBox(width: 10,),
+                AppText(text: "Continue with Google",),
+              ],
+            ),
+          ]),
+          SizedBox(height: 20,),
+          ThirdPartyAuthContainer(children: [
+            Row(
+
+              children: [
+                AppImages.apple(size: 20),
+                SizedBox(width: 10,),
+                AppText(text: "Continue with Apple",),
+              ],
+            ),
+          ])
+        ],
+      ),
+    );
+  }
+}
