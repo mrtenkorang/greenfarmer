444e52ee4a505e447fcc6f4d8a7537d2f444b550
mrtenkorang <josephoforitenkorang390@gmail.com>
2025-03-07 13:05:05 +0000

    m

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Green Farmer',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.kPrimaryColor),
