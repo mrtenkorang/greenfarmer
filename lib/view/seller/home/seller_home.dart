ba1b74a790e1b97657a08e4a05b653d54776fd6d
mrtenkorang <josephoforitenkorang390@gmail.com>
2025-03-07 15:52:05 +0000

    m

--- lib/view/seller/home/seller_home.dart
+++ lib/view/seller/home/seller_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:greenfarmer/view/seller/create_store/create_store.dart';
 import 'package:greenfarmer/view/seller/home/seller_home_controller.dart';
 import 'package:greenfarmer/view/utils/colors.dart';
 import 'package:greenfarmer/view/utils/images.dart';
@@ -8,71 +9,124 @@ import 'package:greenfarmer/view/widgets/shop_card.dart';
 
 import 'package:greenfarmer/view/seller/shop_details/shop_details.dart';
 
-class SellerHome extends StatelessWidget {
-  const SellerHome({super.key});
+class SellerHomeUI extends StatelessWidget {
+  const SellerHomeUI({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final sellerHomeController = Get.put(SellerHomeController());
     return Scaffold(
+      backgroundColor: AppColors.whiteColor,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Get.to(() => CreateShopPage()),
+        child: Icon(Icons.shop),
+      ),
       body: Padding(
         padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top + 20),
         child: Column(
           children: [
             AppTopBar(),
-            SingleChildScrollView(
-              child: Column(
-                children: [
-                  SizedBox(height: 20),
-                  ShopCard(
-                    shopName: "Vegetable shop",
-                    shopImageUrl: AppImages.vegImage2,
-                    onViewShopPressed: () => Get.to(() => ShopDetails()),
-                  ),
-                  SizedBox(height: 20),
-                  ShopCard(
-                    cardColor: AppColors.kShopCardColor2,
-                    shopName: "Fruit shop",
-                    shopImageUrl: AppImages.fruitImage,
-                    onViewShopPressed: () => Get.to(() => ShopDetails()),
-                  ),
-                ],
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    SizedBox(height: 20),
+                    ShopCard(
+                      shopName: "Vegetables shop",
+                      shopImageUrl: AppImages.vegImage2,
+                      onViewShopPressed:
+                          () => Get.to(
+                            () => ShopDetails(
+                              title: "Vegetables shop",
+                              imagePath: AppImages.vegImage2,
+                              inventory: [
+                                {
+                                  "product": "Tomatoes",
+                                  "quantity": "100",
+                                  "price": "1000",
+                                },
+                                {
+                                  "product": "Onions",
+                                  "quantity": "100",
+                                  "price": "1000",
+                                },
+                                {
+                                  "product": "Cabbage",
+                                  "quantity": "100",
+                                  "price": "1000",
+                                },
+                              ],
+                            ),
+                          ),
+                    ),
+                    SizedBox(height: 20),
+                    ShopCard(
+                      spaceWidth: 20,
+                      cardColor: AppColors.kShopCardColor2,
+                      shopName: "Fruit shop",
+                      shopImageUrl: AppImages.fruitImage,
+                      onViewShopPressed:
+                          () => Get.to(
+                            () => ShopDetails(
+                              title: "Fruit Shop",
+                              imagePath: AppImages.fruitImage,
+                              inventory: [
+                                {
+                                  "product": "Apple",
+                                  "quantity": "100",
+                                  "price": "1000",
+                                },
+                                {
+                                  "product": "Banana",
+                                  "quantity": "100",
+                                  "price": "1000",
+                                },
+                                {
+                                  "product": "Orange",
+                                  "quantity": "100",
+                                  "price": "1000",
+                                },
+                              ],
+                            ),
+                          ),
+                    ),
+                    SizedBox(height: 20),
+                    ShopCard(
+                      spaceWidth: 50,
+                      cardColor: AppColors.kShopCardColor,
+                      shopName: "Poultry shop",
+                      shopImageUrl: AppImages.poultryImage,
+                      onViewShopPressed:
+                          () => Get.to(
+                            () => ShopDetails(
+                          title: "Poultry shop",
+                          imagePath: AppImages.poultryImage,
+                              inventory: [
+                                {
+                                  "product": "Chicken",
+                                  "quantity": "100",
+                                  "price": "1000",
+                                },
+                                {
+                                  "product": "Guinea fowl",
+                                  "quantity": "100",
+                                  "price": "1000",
+                                },
+                                {
+                                  "product": "Turkey",
+                                  "quantity": "100",
+                                  "price": "1000",
+                                },
+                              ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                  ],
+                ),
               ),
             ),
           ],
         ),
-      ),
-      bottomNavigationBar: GetBuilder(
-        init: sellerHomeController,
-        builder: (context) {
-          return BottomNavigationBar(
-            items: const [
-              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-              // BottomNavigationBarItem(
-              //   icon: Icon(Icons.location_on),
-              //   label: 'First Evacuation',
-              // ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                label: 'Profile',
-              ),
-            ],
-            currentIndex: sellerHomeController.selectedIndex.value,
-            selectedItemColor: Colors.white,
-            unselectedItemColor: Colors.grey,
-            backgroundColor: AppColors.kPrimaryColor,
-            onTap: sellerHomeController.changeIndex,
-            selectedLabelStyle: TextStyle(
-              fontFamily: 'Montserrat', // Replace with your font family
-              fontSize: 12,
-              fontWeight: FontWeight.bold,
-            ),
-            unselectedLabelStyle: TextStyle(
-              fontFamily: 'Montserrat', // Replace with your font family
-              fontSize: 12, // Adjust the font size as needed
-            ),
-          );
-        },
       ),
     );
   }
